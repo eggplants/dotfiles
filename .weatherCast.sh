e2dfbaae5c1bd55752bdaacd61507a331d5ab057
eggplants <w10776e8w@yahoo.co.jp>
2022-04-06 11:28:33 +0900

    Update .weatherCast.sh

--- .weatherCast.sh
+++ .weatherCast.sh
@@ -1,25 +1,30 @@
 main(){
-    local file fetchdate todaydate chk
+    local file h today
 
     file="$HOME/.weather"
-    : >> "${file}"
-
-    seq -s\# 30 | tr -d 0-9
+    : >> "$file"
+    h="$(seq -s\# 30 | tr -d 0-9)"
+    echo "$h"
     echo '...TODAY WEATHER REPORT...'
-    seq -s\# 30 | tr -d 0-9
+    echo "$h"
 
     # いちいちcurl叩くのは嫌なので一日一回にする
-    fetchdate="$(sed '7!d' ${file})"
-    todaydate="$(LANG=en date +%a%%%d%%%h|tr % \ )"
-    [[ fetchdate = todaydate ]] || {
-        curl -s 'http://wttr.in/tsukuba?FQn1' > "${file}"
-    }
+    today="$(date +%d)"
+    if [[ "$(head -1 "$file")" = "$today" ]]; then
+        tail -15 "$file"
+        return 0
+    fi
+
+    echo "$today" > "$file"
+    curl -s 'http://wttr.in/tsukuba?FQn1' >> "$file"
+
     # curl成功してたら表示
-    [[ -n "$(<${file})" ]] && {
-        cat "${file}" | sed '12s/ │/│/g'
-    } || {
+    if [[ "$(wc -l < "$file")" = "16" ]]; then
+        tail -15 "$file"
+    else
         echo-sd '...OUT OF SURVISE...'
+        : > "$file"
         return 1
-    }
+    fi
 }
 main
